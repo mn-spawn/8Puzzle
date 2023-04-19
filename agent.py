88960c0078dac4490166ec164709bc0515f3a7e1
mn-spawn <madelinespawn@gmail.com>
2023-04-19 00:44:27 -0700

    put in MT heuristic
    A function that compares a correctboard to our board

--- agent.py
+++ agent.py
@@ -7,12 +7,23 @@ from collections.abc import Callable
 Heuristics
 '''
 def MT(board: Board) -> int:
-    return 
+    # go through and count how many non-matches there are
+    i = 1
+    misplaced = 0
+    for row in Board:
+        for column in row:
+            if i != column and column != 9:
+                misplaced += 1
+            i += 1;
+
+    return misplaced
 
 def CB(board: Board) -> int:
+    #for each tile check City Block distance and sum
     return 
 
 def NA(board: Board) -> int:
+    #my heuristic here
     return 
 
 
